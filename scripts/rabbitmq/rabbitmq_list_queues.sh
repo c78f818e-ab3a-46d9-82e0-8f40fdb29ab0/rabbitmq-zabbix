fe9ec5b1e51d72210e18fdb35b0be650dce6b7d7
System <Mr.NN91@gmail.com>
2017-03-10 14:02:16 +0700

    Update and rename list_rabbit_queues.sh to rabbitmq_list_queues.sh

---
+++ scripts/rabbitmq/rabbitmq_list_queues.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#
+cd "$(dirname "$0")"
+. .rab.auth
+
+if [[ -z "$HOSTNAME" ]]; then
+    HOSTNAME=`hostname`
+fi
+if [[ -z "$NODE" ]]; then
+    NODE=`hostname`
+fi
+
+./rabbitmq_api_v1.py --username=$USERNAME --password=$PASSWORD --check=list_queues --filter="$FILTER" --conf=$CONF --hostname=$HOSTNAME --node="$NODE"  --loglevel=${LOGLEVEL} --logfile=${LOGFILE} --port=$PORT --protocol=$PROTOCOL
